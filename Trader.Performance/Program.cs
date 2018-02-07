e11f2d49c2a1c96d0307e356132985d1b6ca6d32
norek <norekzal@gmail.com>
2018-02-07 20:15:19 +0100

    Add performance project

---
+++ Trader.Performance/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Threading.Tasks;
+
+namespace Trader.Performance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<RSI_Strategy_Perf>();
+
+            Console.WriteLine("Whatsuup?");
+        }
+    }
+
+    public class RSI_Strategy_Perf
+    {
+        [Benchmark]
+        public void Do()
+        {
+            Task.Delay(200).Wait();
+        }
+    }
+}
