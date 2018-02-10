be78017950c615854602ad64bc96fde7c1d8e304
norek <norekzal@gmail.com>
2018-02-10 23:57:19 +0100

    add ioc global module

---
+++ Trader/Framework/TraderModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Trader.Backfill;
+using Trader.Exchanges;
+
+namespace Trader.Framework
+{
+    public class TraderModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BackFillStorage>().As<IBackfillStorage>().SingleInstance();
+            builder.RegisterType<Backfill.Backfill>().As<IBackFill>().SingleInstance();
+            builder.RegisterType<Poloniex>().As<IExchange>().SingleInstance();
+            
+            base.Load(builder);
+        }
+    }
+}
