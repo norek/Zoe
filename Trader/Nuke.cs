e3e15927f72b79d78756b080e6752292ee0f8a26
norek <norekzal@gmail.com>
2018-02-10 20:23:51 +0100

    add nuke.cs xD

---
+++ Trader/Nuke.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Trader.Tests")]
+
+namespace Trader
+{
+    //its for nothing special only InternalsVisibleTo marker
+    public class Nuke
+    {
+    }
+}
