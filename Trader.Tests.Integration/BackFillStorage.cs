68de2ae8585abc873ba413bdc82b9b0f46fa73dd
norek <norekzal@gmail.com>
2018-02-10 23:56:41 +0100

    add empty project with integration tests

---
+++ Trader.Tests.Integration/BackFillStorage.cs
@@ -0,0 +1,22 @@
+using Autofac;
+using System;
+using Trader.Backfill;
+using Trader.Framework.Ioc;
+using Xunit;
+
+namespace Trader.Tests.Integration
+{
+    public class BackFillStorage
+    {
+        public BackFillStorage()
+        {
+
+        }
+
+        [Fact]
+        public void BackFillStorage_ShouldBeAble_ToStore_AndLoadWithDateOrder()
+        {
+
+        }
+    }
+}
